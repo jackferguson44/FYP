e1cad629bd5c87879f2bad55465c0efef220198c
Jack ferguson <t7099806@live.tees.ac.uk>
2021-02-18 00:16:27 +0000

    read list working

--- app/src/main/java/uk/ac/tees/t7099806/mediatracker2/ReadListActivity.java
+++ app/src/main/java/uk/ac/tees/t7099806/mediatracker2/ReadListActivity.java
@@ -1,11 +1,22 @@
 package uk.ac.tees.t7099806.mediatracker2;
 
+import android.animation.ValueAnimator;
+import android.net.UrlQuerySanitizer;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,10 +31,26 @@ public class ReadListActivity extends AppCompatActivity {
     private BookInfoFirebase bookInformation;
     private BookListAdapter adapter;
     private ArrayList<BookInfoFirebase> bookInfoFirebaseArrayList;
-
     private LinearLayoutManager linearLayoutManager;
     private RecyclerView recyclerView;
 
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference rootRef;
+    private DatabaseReference listsRef;
+    private String userID;
+
+    ValueEventListener valueEventListener;
+
+    private String publisher;
+    private String noPages;
+    private String publishDate;
+    private String title;
+    private String subTitle;
+    private String description;
+    private String bookImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +61,43 @@ public class ReadListActivity extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
-        bookInformation = new BookInfoFirebase("VIM", "7", "07/09/12", "Tomie", "Junji Collection" ,"Worth the read", "https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api");
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
+        listsRef = rootRef.child("lists").child(userID).child("read list");
+
+
         bookInfoFirebaseArrayList = new ArrayList<>();
-        bookInfoFirebaseArrayList.add(bookInformation);
+        valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot ds : dataSnapshot.getChildren())
+                {
+                    bookImage = ds.child("bookImage").getValue(String.class);
+                    description = ds.child("description").getValue(String.class);
+                    noPages = ds.child("numberOfPages").getValue(String.class);
+                    publishDate = ds.child("publishDate").getValue(String.class);
+                    publisher = ds.child("publisher").getValue(String.class);
+                    subTitle = ds.child("subTitle").getValue(String.class);
+                    title = ds.child("title").getValue(String.class);
+
+
+
+                    bookInformation = new BookInfoFirebase(publisher, noPages, publishDate, title, subTitle, description, bookImage);
+                    bookInfoFirebaseArrayList.add(bookInformation);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        listsRef.addListenerForSingleValueEvent(valueEventListener);
 
         adapter = new BookListAdapter(bookInfoFirebaseArrayList, ReadListActivity.this);
 
@@ -45,10 +106,7 @@ public class ReadListActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
-
-
-
-
-
     }
+
+
 }
