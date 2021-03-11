5a5ac709204780aae41247b71b0d9c7b4cbb4018
Jack ferguson <t7099806@live.tees.ac.uk>
2021-03-11 00:44:10 +0000

    added splash screen

---
+++ app/src/main/java/uk/ac/tees/t7099806/mediatracker2/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package uk.ac.tees.t7099806.mediatracker2;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Handler;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, 3000);
+
+    }
+}
